c1fa104ae74ff238ba03ca61646518dd67ac830d
Bryce <bryce.sayers-kwan@live.ca>
2017-07-05 01:40:52 +0000

    Fixed get request from hard coded to :id

--- express_server.js
+++ express_server.js
@@ -1,20 +1,26 @@
 const express = require('express');
 const app = express();
+// const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 8080;
-"use strict";
+'use strict';
 app.set('view engine', 'ejs');
 
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  "9sm5xK": "http://www.google.com",
 };
 
 app.get('/urls', (req, res) => {
   res.render('urls_index', { urls: urlDatabase });
 });
 
-app.get('/urls/b2xVn2', (req, res) => {
-  res.render('urls_shows', { shortURL: urlDatabase.b2xVn2 });
+app.get('/urls/:id', (req, res) => {
+  var longURL = urlDatabase[req.params.id];
+  var templateVars = {
+    shortURL: req.params.id,
+    longURL: longURL
+  };
+  res.render('urls_shows', templateVars);
 });
 
 app.listen(PORT, () => {
