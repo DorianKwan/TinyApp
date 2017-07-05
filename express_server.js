b398136e769d805e7987448dffef6f8d51605262
Bryce <bryce.sayers-kwan@live.ca>
2017-07-05 20:34:22 +0000

    Added a delete button to remove urls from the object memory database

--- express_server.js
+++ express_server.js
@@ -4,14 +4,20 @@ const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 8080;
 'use strict';
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
 
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
+  "2b4dwD": "http://www.youtube.com"
 };
 
 app.get('/urls', (req, res) => {
   res.render('urls_index', { urls: urlDatabase });
+});
+
+app.get('/urls/new', (req, res) => {
+  res.render('urls_new');
 });
 
 app.get('/urls/:id', (req, res) => {
@@ -23,18 +29,35 @@ app.get('/urls/:id', (req, res) => {
   res.render('urls_show', templateVars);
 });
 
-app.get('/urls/new', (req, res) => {
-  res.render('urls_new');
-});
-
 app.post('/urls', (req, res) => {
-  console.log(req.body);
-  res.send('Ok');
+  let shortURL = generateRandomString();
+  urlDatabase[shortURL] = req.body.longURL;
+  res.redirect(`urls/${shortURL}`);
 });
 
 function generateRandomString() {
   return Math.random().toString(36).substr(2, 6);
 }
+
+app.post('/urls/:id/delete', (req, res) => {
+  let url = urlDatabase[req.params.id];
+  if (url) {
+    delete urlDatabase[req.params.id];
+    res.redirect('/urls')
+  } else {
+    res.send('The URL you are trying to reach does not exist');
+  }
+});
+
+app.get('/u/:shortURL', (req, res) => {
+  let longURL = urlDatabase[req.params.shortURL];
+
+  if (longURL) {
+    res.redirect(longURL);
+  } else {
+    res.render('urls_not_found');
+  }
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
